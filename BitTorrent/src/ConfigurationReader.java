c31aeeec3bf4513d8934041e420eda4c0c6455d0
dvarik <dvarik@dvarik-PC>
2017-02-18 02:21:56 -0500

    Added configuration reader class and config files.

--- BitTorrent/src/ConfigurationReader.java
+++ BitTorrent/src/ConfigurationReader.java
@@ -1,4 +1,71 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class ConfigurationReader {
+
+	private HashMap<Integer, PeerConfig> peerProps = new HashMap<Integer, PeerConfig>();
+	private HashMap<String, String> commonProps = new HashMap<String, String>();
+
+	private final String peerInfoFileName = "PeerInfo.cfg";
+	private final String commonFileName = "Common.cfg";
+
+	private static ConfigurationReader instance = null;
+
+	public static synchronized ConfigurationReader getInstance() {
+		
+		if (instance == null)
+			instance = new ConfigurationReader();
+		
+		return instance;
+	}
+
+	private ConfigurationReader() {
+
+		try {
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(getClass().getResourceAsStream(commonFileName)));
+
+			String line = null;
+		
+			while ((line = br.readLine()) != null) {
+				String[] split = line.split(" ");
+				commonProps.put(split[0], split[1]);
+			}
+
+			br.close();
+
+			br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(peerInfoFileName)));
+			
+			line = null;
+			while ((line = br.readLine()) != null) {
+			
+				String[] split = line.split(" ");
+				int peerId = Integer.parseInt(split[0]);
+				PeerConfig peer = new PeerConfig(peerId, split[1], Integer.parseInt(split[2]),
+						Integer.parseInt(split[3]));
+				peerProps.put(peerId, peer);
+			}
+			
+			br.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			// add logging
+		} catch (IOException e) {
+			e.printStackTrace();
+			// add logging
+		}
+	}
+
+	public HashMap<String, String> getCommonProps() {
+		return this.commonProps;
+	}
+
+	public HashMap<Integer, PeerConfig> getPeerInfo() {
+		return this.peerProps;
+	}
 
 }
---
+++ BitTorrent/src/PeerConfig.java
@@ -0,0 +1,50 @@
+
+public class PeerConfig {
+
+	int peerId;
+	String hostName;
+	int port;
+	int hasFile;
+
+	public PeerConfig(int peerId, String hostName, int port, int hasFile) {
+		super();
+		
+		this.peerId = peerId;
+		this.hostName = hostName;
+		this.port = port;
+		this.hasFile = hasFile;
+	}
+
+	public int getPeerId() {
+		return peerId;
+	}
+
+	public void setPeerId(int peerId) {
+		this.peerId = peerId;
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public int getHasFile() {
+		return hasFile;
+	}
+
+	public void setHasFile(int hasFile) {
+		this.hasFile = hasFile;
+	}
+
+}
