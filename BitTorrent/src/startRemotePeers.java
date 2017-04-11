a8b253c917483f664e513a372003ebe20e45bca4
dvarik <dvarik@dvarik-PC>
2017-04-11 13:48:21 -0400

    changed the script to work stand alone

--- BitTorrent/src/startRemotePeers.java
+++ BitTorrent/src/startRemotePeers.java
@@ -1,24 +1,44 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.HashMap;
-
+import java.io.InputStreamReader;
+import java.util.TreeMap;
 
 public class startRemotePeers {
 
 	public static void main(String[] args) throws IOException {
 
+		startRemotePeers st = new startRemotePeers();
+		TreeMap<Integer, String> peers = st.getPeers();
+		String workingDir = System.getProperty("user.dir");
+		String peerProcessName = new String("java peerProcess");
 
-	HashMap<Integer,PeerConfig> map = ConfigurationReader.getInstance().getPeerInfo();
-	String workingDir = System.getProperty("user.dir");
-	String peerProcessName = new String("java peerProcess");
+		for (Integer peerId : peers.keySet()) {
+			String hostname = peers.get(peerId);
 
-	for (Integer peerId : map.keySet())
-	{
-		String hostname = map.get(peerId).getHostName();
-		String peerProcessArguments = Integer.toString(peerId);
-
-		Runtime.getRuntime().exec("ssh " + hostname + " cd " + workingDir + " ; " +
-				peerProcessName + " " + peerProcessArguments );
+			Runtime.getRuntime().exec("ssh " + hostname + " cd " + workingDir + " ; " + peerProcessName + " " + peerId);
 		}
+
+	}
+
+	public TreeMap<Integer, String> getPeers() {
+
+		TreeMap<Integer, String> ret = new TreeMap<Integer, String>();
+		try {
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(getClass().getResourceAsStream("PeerInfo.cfg")));
+
+			String line = null;
+
+			while ((line = br.readLine()) != null) {
+
+				String[] split = line.split(" ");
+				int peerId = Integer.parseInt(split[0]);
+				ret.put(peerId, split[1]);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ret;
 
 	}
 
